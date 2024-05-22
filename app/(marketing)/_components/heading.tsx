4c0e63816e75eebe216a21c6790d584b9a85e786
Towkir <towkirmiah5@gmail.com>
2024-05-22 14:46:40 -0400

    Authentication & Database finished

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,7 +14,10 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-	@@ -14,10 +21,27 @@ export const Heading = () => {
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Jotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
