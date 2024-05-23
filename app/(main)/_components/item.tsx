ee1254adf3dd16106f10ac8cf94a2402b56985aa
Towkir <towkirmiah5@gmail.com>
2024-05-23 13:34:15 -0400

    Added in schema & api

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+interface itemProps {
+    label: string;
+    onClick: () => void;
+    icon: LucideIcon;
+}
+
+export const Item = () => {
+    return (
+        <div>
+            Item
+        </div>
+    )
+}
